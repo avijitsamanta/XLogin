2ec73ae4046b15a0eeaf508f1f60a615ab6ee0f2
avijitsamanta <avijit.tict@gmail.com>
2024-01-28 18:52:46 +0530

    login form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from "react";
+
 
 function App() {
+  const [inputval, setInputval] = useState({
+    username:"",
+    password:""
+  });
+  const [msg, setMsg] = useState('');
+  const [errorMsg, setErrorMsg] = useState('');
+
+  const handleChange = (event) => {
+    const {name,value} = event.target;
+    setInputval({...inputval,[name]:value});
+  };
+
+  console.log(inputval)
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    let errMsg = "Invalid username or password";
+    let successMsg = "";
+    const {username,password} = inputval;
+    if(username === "user" && password === "password"){
+      successMsg = `Welcome, ${username}!`;
+      setMsg(successMsg)
+      setErrorMsg("");
+    }else{
+      setErrorMsg(errMsg);
+      setMsg("")
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Login Page</h1>
+      {errorMsg && (
+        <div>
+          <p>{errorMsg}</p>
+        </div>
+      )}
+      {msg ? (
+        <div>
+          <p>{msg}</p>
+        </div>
+      ):(
+        <form onSubmit={handleFormSubmit}>
+        <div>
+          <label>
+            Username:
+          </label>
+          <input
+              type="text"
+              placeholder="username"
+              name="username"
+              value={inputval.username}
+              onChange={handleChange}
+              required
+            />
+        </div>
+        <div>
+          <label>
+            Password:
+          </label>
+          <input
+              type="password"
+              placeholder="password"
+              name="password"
+              value={inputval.password}
+              onChange={handleChange}
+              required
+            />
+        </div>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+      )}
     </div>
-  );
+  )
 }
 
 export default App;
